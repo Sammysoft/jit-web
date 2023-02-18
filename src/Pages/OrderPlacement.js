2482082ad006e015899d41c97ccb36bb1da68d8b
Samuel Dare-Owonibi <dareowonibisamuel@gmail.com>
2023-02-18 16:03:48 +0000

    did a lot of other implementation

--- src/Pages/OrderPlacement.js
+++ src/Pages/OrderPlacement.js
@@ -7,6 +7,8 @@ import back from "../Res/Icons/back.svg";
 
 const OrderPlacementScreen = () => {
   const [variant, setVariant] = useState("");
+  const [delivery, setDelivery]= useState("")
+  const [gram, setGram] = useState("");
   const [index, setIndex] = useState(0);
 
   return (
@@ -43,7 +45,21 @@ const OrderPlacementScreen = () => {
                     Flora
                   </Card>
                 )}
-                {variant === "Good Mama | Flora" && <Card chosen={true}>Flora</Card>}
+                {variant === "Good Mama | Flora" && (
+                  <Card chosen={true}>Flora</Card>
+                )}
+                {variant !== "Good Mama | Lemon" && (
+                  <Card
+                    onClick={() => {
+                      setVariant("Good Mama | Lemon");
+                    }}
+                  >
+                    Lemon
+                  </Card>
+                )}
+                {variant === "Good Mama | Lemon" && (
+                  <Card chosen={true}>Lemon</Card>
+                )}
                 {variant !== "Good Mama | Ankara" && (
                   <Card
                     onClick={() => {
@@ -53,7 +69,9 @@ const OrderPlacementScreen = () => {
                     Ankara
                   </Card>
                 )}
-                {variant === "Good Mama | Ankara" && <Card chosen={true}>Ankara</Card>}
+                {variant === "Good Mama | Ankara" && (
+                  <Card chosen={true}>Ankara</Card>
+                )}
                 {variant !== "Good Mama | Diamond" && (
                   <Card
                     onClick={() => {
@@ -63,7 +81,9 @@ const OrderPlacementScreen = () => {
                     Diamond
                   </Card>
                 )}
-                {variant === "Good Mama | Diamond" && <Card chosen={true}>Diamond</Card>}
+                {variant === "Good Mama | Diamond" && (
+                  <Card chosen={true}>Diamond</Card>
+                )}
               </div>
               <Variant>So klin</Variant>
               <div
@@ -83,7 +103,9 @@ const OrderPlacementScreen = () => {
                     Ultra
                   </Card>
                 )}
-                {variant === "So klin | Ultra" && <Card chosen={true}>Ultra</Card>}
+                {variant === "So klin | Ultra" && (
+                  <Card chosen={true}>Ultra</Card>
+                )}
                 {variant !== "So klin | Protect" && (
                   <Card
                     onClick={() => {
@@ -93,7 +115,9 @@ const OrderPlacementScreen = () => {
                     Protect
                   </Card>
                 )}
-                {variant === "So klin | Protect" && <Card chosen={true}>Protect</Card>}
+                {variant === "So klin | Protect" && (
+                  <Card chosen={true}>Protect</Card>
+                )}
                 {variant !== "So klin | Boom" && (
                   <Card
                     onClick={() => {
@@ -103,7 +127,9 @@ const OrderPlacementScreen = () => {
                     Boom
                   </Card>
                 )}
-                {variant === "So klin | Boom" && <Card chosen={true}>Boom</Card>}
+                {variant === "So klin | Boom" && (
+                  <Card chosen={true}>Boom</Card>
+                )}
               </div>
             </div>
 
@@ -129,15 +155,35 @@ const OrderPlacementScreen = () => {
                 width: "100%",
               }}
             >
-                <Variant>{variant}</Variant>
+              <Variant>{variant}</Variant>
+              <SelectorWrap>
+                <Selector>How many grams do you want?</Selector>
+                <Selector>
+                  <Select
+                    placeholder="Select Grammage"
+                    value={gram}
+                    onChange={(e) => {
+                      setGram(e.target.value);
+                    }}
+                  >
+                    <Option value={"22 | 3.69(kg)"}>22 | 3.69(kg)</Option>
+                    <Option value={"80 | 4.00(kg)"}>80 | 4.00(kg)</Option>
+                    <Option value={"90 | 4.50(kg)"}>90 | 4.50(kg)</Option>
+                    <Option value={"170 | 4.42(kg)"}>170 | 4.42(kg)</Option>
+                    <Option value={"450 | 5.40(kg)"}>450 | 5.40(kg)</Option>
+                    <Option value={"850 | 5.10(kg)"}>850 | 5.10(kg)</Option>
+                    <Option value={"935 | 5.61(kg)"}>935 | 5.61(kg)</Option>
+                  </Select>
+                </Selector>
+              </SelectorWrap>
             </div>
-             <NextWrap
+            <NextWrap
               style={{ float: "left" }}
               onClick={() => {
                 setIndex(index - 1);
               }}
             >
-             <img src={back} alt="next" />  Back
+              <img src={back} alt="next" /> Back
             </NextWrap>
             <NextWrap
               onClick={() => {
@@ -148,11 +194,133 @@ const OrderPlacementScreen = () => {
             </NextWrap>
           </>
         )}
+        {index === 2 && (
+          <>
+            <HeadText>Set Delivery Location</HeadText>
+            <div
+              style={{
+                height: "80vh",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "flex-start",
+                justifyContent: "center",
+                width: "100%",
+              }}
+            >
+              <Variant>{variant}</Variant>
+              <Variant>{gram}</Variant>
+              <SelectorWrap>
+                <Selector>Where should we drop your package?</Selector>
+                <Selector>
+                  <Input
+                    placeholder="Enter delivery Address"
+                    value={delivery}
+                    onChange={(e) => {
+                      setDelivery(e.target.value);
+                    }}
+                  />
+                </Selector>
+              </SelectorWrap>
+            </div>
+            <NextWrap
+              style={{ float: "left" }}
+              onClick={() => {
+                setIndex(index - 1);
+              }}
+            >
+              <img src={back} alt="next" /> Back
+            </NextWrap>
+            <NextWrap
+              onClick={() => {
+                setIndex(index + 1);
+              }}
+            >
+              Done
+            </NextWrap>
+          </>
+        )}
+        {index === 3 && (
+          <>
+            <HeadText>Confirm Order</HeadText>
+            <div
+              style={{
+                height: "80vh",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "flex-start",
+                justifyContent: "center",
+                width: "100%",
+              }}
+            >
+              <SelectorWrap>
+                <Selector>
+                  Is this exactly what you want to be delivered?
+                </Selector>
+                <Selector>
+                  <Variant>{variant}</Variant>
+                  <Variant>{gram}</Variant>
+                  <Variant>To be delivered at, {delivery}</Variant>
+                </Selector>
+              </SelectorWrap>
+            </div>
+            <NextWrap
+              style={{ float: "left" }}
+              onClick={() => {
+                setIndex(index - 1);
+              }}
+            >
+              <img src={back} alt="next" /> Back
+            </NextWrap>
+            <NextWrap
+              onClick={() => {
+                setIndex(index + 1);
+              }}
+            >
+              Done
+            </NextWrap>
+          </>
+        )}
       </Wrapper>
       <BottomNavBar />
     </>
   );
 };
+
+const Input = styled.input`
+  width: 90%;
+  padding: 10px;
+  border-radius: 10px;
+  font-family: ${Fonts.Laila};
+  margin-bottom: 10px;
+  border: 1px solid ${Colors.PRIMARY};
+`;
+
+const SelectorWrap = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Selector = styled.div`
+  font-family: ${Fonts.Laila};
+  width: 100%;
+  text-align: center;
+`;
+const Select = styled.select`
+  width: 60%;
+  padding: 10px;
+  font-family: ${Fonts.Laila};
+  border-radius: 10px;
+  border: 1px solid ${Colors.PRIMARY};
+  background-color: ${Colors.PRIMARY};
+  color: #ffffff;
+`;
+const Option = styled.option`
+  font-family: ${Fonts.Laila};
+  color: #ffffff;
+`;
 
 const NextWrap = styled.div`
   width: 20%;
@@ -181,7 +349,7 @@ const Variant = styled.div`
   font-size: 1rem;
   opacity: 0.5;
   text-align: left;
-  padding: 15px 0px 15px 0px;
+  padding: 10px 0px 10px 0px;
 `;
 
 const Card = styled.div`
